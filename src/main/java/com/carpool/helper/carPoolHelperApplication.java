00171d582318f5fe1bdf242ec7f0232e1f12a0d9
Magesh90 <mageshb@live.com>
2015-12-14 00:53:16 -0600

    testing the local repo

--- src/main/java/com/carpool/helper/carPoolHelperApplication.java
+++ src/main/java/com/carpool/helper/carPoolHelperApplication.java
@@ -8,6 +8,6 @@ public class carPoolHelperApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(carPoolHelperApplication.class, args);
-        System.out.println("Hello CarPool App");
+        System.out.println("Hello CarPool Application");
     }
 }
