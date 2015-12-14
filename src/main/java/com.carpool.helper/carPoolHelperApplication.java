199411d38ce6103ef71f74ce8f908c5ffaddb1ae
Vijaya.Narayanan <a519271@34363bc8aea4.hq.target.com>
2015-12-13 23:14:39 -0600

    updated POM

---
+++ src/main/java/com.carpool.helper/carPoolHelperApplication.java
@@ -0,0 +1,12 @@
+package com.carpool.helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class carPoolHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(carPoolHelperApplication.class, args);
+    }
+}
